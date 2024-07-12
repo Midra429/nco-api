781711f55957901ac32e1e0c0c8aa2ba3724bd05
Midra <me@midra.me>
2024-07-12 16:51:53 +0900

    適用済みのほう返してなかった

--- src/utils/applyNgSetting.ts
+++ src/utils/applyNgSetting.ts
@@ -10,6 +10,10 @@ export const applyNgSetting = (
   threads: V1Thread[],
   ngSetting: NgSetting
 ): V1Thread[] => {
+  if (!Object.values(ngSetting).flat().length) {
+    return threads
+  }
+
   const applied: V1Thread[] = []
 
   for (const thread of threads) {
@@ -44,5 +48,5 @@ export const applyNgSetting = (
     applied.push({ ...thread, comments })
   }
 
-  return threads
+  return applied
 }
