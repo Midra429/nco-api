fd45a5992b485a17541fa80f1f11611d2c35c9a9
Midra <me@midra.me>
2024-07-18 17:16:57 +0900

    tokenを外から渡すように

--- src/abema/program.ts
+++ src/abema/program.ts
@@ -2,29 +2,28 @@ import type { Program } from '../types/abema/program'
 
 const API_BASE_URL = 'https://api.p-c3-e.abema-tv.com/v1/video/programs/'
 
-export const program = async (id: string): Promise<Program | null> => {
-  const token = localStorage.getItem('abm_token')
+export const program = async (
+  id: string,
+  token: string
+): Promise<Program | null> => {
+  const url = new URL(id, API_BASE_URL)
 
-  if (token) {
-    const url = new URL(id, API_BASE_URL)
+  url.searchParams.set('division', '0')
+  url.searchParams.set('include', 'tvod')
 
-    url.searchParams.set('division', '0')
-    url.searchParams.set('include', 'tvod')
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    const json: Program = await response.json()
 
-    try {
-      const response = await fetch(url, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      const json: Program = await response.json()
-
-      if (json) {
-        return json
-      }
-    } catch (err) {
-      console.error('[nco-api/abema/program]', err)
+    if (json) {
+      return json
     }
+  } catch (err) {
+    console.error('[nco-api/abema/program]', err)
   }
 
   return null
--- src/fod/episode.ts
+++ src/fod/episode.ts
@@ -1,33 +1,29 @@
 import type { Episode } from '../types/fod/episode'
 
-import { getCookie } from '../utils/getCookie'
-
 const API_BASE_URL = 'https://i.fod.fujitv.co.jp/apps/api/episode/detail'
 
-export const episode = async (id: string): Promise<Episode | null> => {
-  const token = getCookie('CT')
+export const episode = async (
+  id: string,
+  token: string
+): Promise<Episode | null> => {
+  const url = new URL(API_BASE_URL)
 
-  if (token) {
-    const url = new URL(API_BASE_URL)
+  url.searchParams.set('ep_id', id)
+  url.searchParams.set('is_premium', 'false')
 
-    url.searchParams.set('ep_id', id)
-    url.searchParams.set('is_premium', 'false')
-    url.searchParams.set('dv_type', 'web')
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'X-Authorization': `Bearer ${token}`,
+      },
+    })
+    const json: Episode = await response.json()
 
-    try {
-      const response = await fetch(url, {
-        headers: {
-          'X-Authorization': `Bearer ${token}`,
-        },
-      })
-      const json: Episode = await response.json()
-
-      if (json) {
-        return json
-      }
-    } catch (err) {
-      console.error('[nco-api/fod/episode]', err)
+    if (json) {
+      return json
     }
+  } catch (err) {
+    console.error('[nco-api/fod/episode]', err)
   }
 
   return null
